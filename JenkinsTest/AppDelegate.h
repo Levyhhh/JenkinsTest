ae8fbdf63f7c29b70c401d89b97ad18e3d7693b4
huliwu <huliwu@feinno.com>
2014-12-03 18:06:58 +0800

    test

---
+++ JenkinsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by Liv on 14/12/3.
+//  Copyright (c) 2014年 胡利武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by Liv on 14/12/3.
+//  Copyright (c) 2014年 胡利武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
